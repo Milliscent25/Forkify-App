380742f3554d722c8d92e8483862fc786dda1b1b
Milliscent25 <milliscent@uncommon.org>
2023-02-14 17:37:13 +0200

    implementing error and success messagest

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+//i this file we weill put all the variables that should be consistence/ should be reused across th project
+
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+
+export const TIMEOUT_SEC = 10;
+
---
+++ src/js/model.js
@@ -0,0 +1,40 @@
+import { async } from "regenerator-runtime";
+import {API_URL} from './config';
+import {getJSON} from './helpers' 
+
+export const state = {
+    recipe: {},
+};
+
+export const loadRecipe = async function (id) {
+    try {
+        const data = await fetch(`${API_URL}/${id}`)
+
+        const res = await fetch(`${API_URL}/${id}`
+
+        );
+
+        // const data = await res.json();
+        // if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+
+        const { recipe } = data.data;
+        state.recipe = {
+            id: recipe.id,
+            title: recipe.title,
+            publisher: recipe.publisher,
+            sourceUrl: recipe.source_url,
+            image: recipe.image_url,
+            servings: recipe.servings,
+            cookingTime: recipe.cooking_time,
+            ingredients: recipe.ingredients,
+
+        }
+        console.log(state.recipe);
+    } catch (err) {
+        
+        //temp error handling 
+        console.err(`${err}`);
+    }
+
+};
